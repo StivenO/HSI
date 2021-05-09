a97617f2a7272560f5df1ddccde2529e8e9c2962
Acosticahub <acostabuitrago0123@gmail.com>
2021-05-08 23:35:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -573,6 +573,8 @@
         </div>
     </div>
 
+    
+
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
     <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
