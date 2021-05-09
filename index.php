e1d25b4dce247ac5550fcd33406b3b7d9fe0fc4c
Acosticahub <acostabuitrago0123@gmail.com>
2021-05-09 01:38:44 -0500

    Cambio 10
    Quité el botón de Generar Reportes de la Página Principal

--- index.php
+++ index.php
@@ -400,9 +400,7 @@
                     <!-- Encabezado de página -->
                     <div class="d-sm-flex align-items-center justify-content-between mb-4">
                         <h1 class="h3 mb-0 text-gray-800">Página Principal</h1>
-                        <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
-                                class="fas fa-download fa-sm text-white-50"></i>Generar Reporte</a>
-                    </div>
+                    </div><br>
 
                     <!-- Fila de contenido -->
                     <div class="row">
@@ -573,7 +571,7 @@
         </div>
     </div>
 
-    
+
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
